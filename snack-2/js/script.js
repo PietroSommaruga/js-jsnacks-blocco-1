45ad084a4dae5b6d365208f5cd4e678f37f84b39
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2021-12-15 15:53:05 +0100

    snack-2

---
+++ snack-2/js/script.js
@@ -0,0 +1,8 @@
+// definsico N con un prompt
+const N = parseInt (prompt('inserisca un numero'));
+
+// apro un ciclo for per ottenere i valori di i da 0 a N
+for(let i = 0 ; i <= N; i++){
+    // stampo in console tutti gli i elevati alla terza (**)
+    console.log(i**3)
+}
