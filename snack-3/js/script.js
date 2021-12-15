194f4f54d7792b5b76011a5c4575dddc31f3b284
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2021-12-15 15:59:25 +0100

    snack-3

---
+++ snack-3/js/script.js
@@ -0,0 +1,8 @@
+// definsico N 
+const N = 2;
+
+// apro un ciclo for per ottenere i valori di i da 0 a 1000
+for(let i = 0 ; i <= 1000; i++){
+    // stampo in console tutti gli N elevati fino alla 1000 (**)
+    console.log(N**i)
+}
